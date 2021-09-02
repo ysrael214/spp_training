60d27fb29de098506309141f95c8bdf774faf469
Menard Ysrael Cruz <ysrael214@gmail.com>
2021-09-02 22:43:36 +0800

    first commit

---
+++ config/modules.config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\I18n',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Form',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Application',
+    'Album'
+];
