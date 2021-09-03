6f881284388308dfe8acb834c667b702afdcf617
Menard Ysrael Cruz <ysrael214@gmail.com>
2021-09-03 23:54:59 +0800

    Replaced calculation with moment.js

--- public/js/training/age_calculator.js
+++ public/js/training/age_calculator.js
@@ -1,15 +1,13 @@
 function updateDate(){
     let dateVal = $('.input-date').val();
-    dateVal = new Date(dateVal);
-    let age = Math.abs( new Date() - dateVal )/1000/3600/24/365;
+    let age =  moment().diff(dateVal, 'years');
     $(".input-age").val(Math.floor(age));
 }
 
 $( document ).ready(function() {
     $("tr").each(function(){
         let rowDate = $(this).find(".date").html();
-        rowDate = new Date(rowDate);
-        let age = Math.abs( new Date() - rowDate )/1000/3600/24/365;
+        let age =  moment().diff(rowDate, 'years');
         $(this).find(".age").html(Math.floor(age));
     });
 
